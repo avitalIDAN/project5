ccb13bfd203d1f12387ead07dbf2a2f02ad9f861
אביטל עי <tchyk52@gmail.com>
2023-05-16 00:12:46 +0300

    todo and post

--- src/pages/ViewPostsUser.js
+++ src/pages/ViewPostsUser.js
@@ -1,74 +1,218 @@
-import React, { Component} from "react";
+// import React, { Component} from "react";
+// import './ViewPostsUser.css';
+
+
+// class ViewPostsUser extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.boldThePost = this.boldThePost.bind(this);
+//       this.showComments = this.showComments.bind(this);    
+//       this.getCurrentComments = this.getCurrentComments.bind(this);  
+//       this.data={
+//         CommentForPost:[]
+//        };  
+//     }
+
+//     async getCurrentComments(id){
+//       await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+//         .then(response => {if(response.ok) {
+//                               return response.json();    
+//                           } else{
+//                              throw "Request failed!";    
+//                       }
+//           })
+//         .then(listComments=> {if(listComments.length==0){
+//                           throw "You have not Comments";
+//                       }else{
+//                         this.data.CommentForPost=listComments;
+//                         return listComments;
+//                       }
+//           })
+//         .catch(error=>alert(""+error));
+//     }
+
+//     async showComments(id){
+//       var comments= document.getElementById("forComments");
+//       await this.getCurrentComments(id);
+//       this.data.CommentForPost.map((comment)=>(
+//         comments.innerHTML+= comment.name + "\r\n" + comment.body+ "\r\n"
+//       ));
+//     }
+    
+//     boldThePost(id,e){
+//       var body= document.getElementById("postBody");
+//       var title= document.getElementById("title");
+//       //title.style.fontWeight="bold";
+
+//       var currentPost= this.props.listPosts[id-1];
+//       body.innerText=currentPost.body;
+
+//        var button = document.createElement("button");
+//        button.addEventListener('click',(e) => {button.innerHTML="Hide the comments &#xf086"; this.showComments(id, e);});
+//        button.setAttribute("id", "btnComments");
+//        button.setAttribute("class", 'fas');
+//        button.innerHTML = "View the comments &#xf086";
+//        body.appendChild(button);
+//     }
+
+//     render() {
+//       return (  
+//         <div>
+//             {this.props.listPosts.map((post)=>(
+//             <div>
+//                 <button id="title" onClick={(e) => this.boldThePost(post.id, e)}>{post.id}. {post.title}</button>  
+//                 <p id="postBody" ></p> 
+//                 <div id="forBtn"></div>   
+//                 <div id="forComments"></div>
+//              </div>        
+//           ))}
+//         </div>
+//       );
+//     }
+//   }
+   
+//   export default ViewPostsUser;
+
+
+//--------------------------
+// import React, { useState, useEffect } from "react";
+// import './ViewPostsUser.css';
+
+// const ViewPostsUser = ({ listPosts }) => {
+//   const [selectedPost, setSelectedPost] = useState(null);
+//   const [comments, setComments] = useState([]);
+
+//   const getCurrentComments = async (id) => {
+//     try {
+//       const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
+//       if (!response.ok) {
+//         throw new Error("Request failed!");
+//       }
+//       const listComments = await response.json();
+//       if (listComments.length === 0) {
+//         throw new Error("You have no comments");
+//       }
+//       setComments(listComments);
+//     } catch (error) {
+//       alert("" + error);
+//     }
+//   };
+
+//   const showComments = async (id) => {
+//     await getCurrentComments(id);
+//   };
+
+//   const boldThePost = (id) => {
+//     setSelectedPost(id);
+//   };
+
+//   useEffect(() => {
+//     setComments([]);
+//   }, [selectedPost]);
+
+//   return (
+//     <div>
+//       {listPosts.map((post) => (
+//         <div key={post.id}>
+//           <button
+//             id="title"
+//             onClick={() => boldThePost(post.id)}
+//             className={selectedPost === post.id ? "highlighted" : ""}
+//           >
+//             {post.id}. {post.title}
+//           </button>
+//           {selectedPost === post.id && (
+//             <>
+//               <p id="postBody">{post.body}</p>
+//               <button id="btnComments" className="fas" onClick={() => showComments(post.id)}>
+//                 View the comments &#xf086;
+//               </button>
+//             </>
+//           )}
+//           {selectedPost === post.id && comments.length > 0 && (
+//             <div id="forComments">
+//               {comments.map((comment) => (
+//                 <p key={comment.id}>
+//                   {comment.name} <br /> {comment.body}
+//                 </p>
+//               ))}
+//             </div>
+//           )}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default ViewPostsUser;
+
+import React, { useState, useEffect } from "react";
 import './ViewPostsUser.css';
 
+const ViewPostsUser = ({ listPosts }) => {
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
-class ViewPostsUser extends Component {
-    constructor(props) {
-      super(props);
-      this.boldThePost = this.boldThePost.bind(this);
-      this.showComments = this.showComments.bind(this);    
-      this.getCurrentComments = this.getCurrentComments.bind(this);  
-      this.data={
-        CommentForPost:[]
-       };  
+  const getCurrentComments = async (id) => {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+      const listComments = await response.json();
+      if (listComments.length === 0) {
+        throw new Error("You have no comments");
+      }
+      setComments(listComments);
+    } catch (error) {
+      alert("" + error);
     }
+  };
 
-    async getCurrentComments(id){
-      await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
-        .then(response => {if(response.ok) {
-                              return response.json();    
-                          } else{
-                             throw "Request failed!";    
-                      }
-          })
-        .then(listComments=> {if(listComments.length==0){
-                          throw "You have not Comments";
-                      }else{
-                        this.data.CommentForPost=listComments;
-                        return listComments;
-                      }
-          })
-        .catch(error=>alert(""+error));
-    }
+  const showComments = async (id) => {
+    await getCurrentComments(id);
+  };
 
-    async showComments(id){
-      var comments= document.getElementById("forComments");
-      await this.getCurrentComments(id);
-      this.data.CommentForPost.map((comment)=>(
-        comments.innerHTML+= comment.name + "\r\n" + comment.body+ "\r\n"
-      ));
-    }
-    
-    boldThePost(id,e){
-      var body= document.getElementById("postBody");
-      var title= document.getElementById("title");
-      //title.style.fontWeight="bold";
+  const togglePost = (id) => {
+    setSelectedPost(prevSelectedPost => prevSelectedPost === id ? null : id);
+  };
 
-      var currentPost= this.props.listPosts[id-1];
-      body.innerText=currentPost.body;
+  useEffect(() => {
+    setComments([]);
+  }, [selectedPost]);
 
-       var button = document.createElement("button");
-       button.addEventListener('click',(e) => {button.innerHTML="Hide the comments &#xf086"; this.showComments(id, e);});
-       button.setAttribute("id", "btnComments");
-       button.setAttribute("class", 'fas');
-       button.innerHTML = "View the comments &#xf086";
-       body.appendChild(button);
-    }
-
-    render() {
-      return (  
-        <div>
-            {this.props.listPosts.map((post)=>(
-            <div>
-                <button id="title" onClick={(e) => this.boldThePost(post.id, e)}>{post.id}. {post.title}</button>  
-                <p id="postBody" ></p> 
-                <div id="forBtn"></div>   
-                <div id="forComments"></div>
-             </div>        
-          ))}
+  return (
+    <div>
+      {listPosts.map((post) => (
+        <div key={post.id}>
+          <button
+            id="title"
+            onClick={() => togglePost(post.id)}
+            className={selectedPost === post.id ? "highlighted" : ""}
+          >
+            {post.id}. {post.title}
+          </button>
+          {selectedPost === post.id && (
+            <>
+              <p id="postBody">{post.body}</p>
+              <button id="btnComments" className="fas" onClick={() => showComments(post.id)}>
+                View the comments &#xf086;
+              </button>
+            </>
+          )}
+          {selectedPost === post.id && comments.length > 0 && (
+            <div id="forComments">
+              {comments.map((comment) => (
+                <p key={comment.id}>
+                  {comment.name} <br /> {comment.body}
+                </p>
+              ))}
+            </div>
+          )}
         </div>
-      );
-    }
-  }
-   
-  export default ViewPostsUser;
+      ))}
+    </div>
+  );
+};
+
+export default ViewPostsUser;
--- src/pages/ViewTodosUser.js
+++ src/pages/ViewTodosUser.js
@@ -1,61 +1,168 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class ViewTodosUser extends Component {
-    constructor(props) {
-      super(props);
-      this.sortByABC = this.sortByABC.bind(this);  
-      this.changeFunc = this.changeFunc.bind(this);  
-      this.data={
-        listTitles:[]
-      };
-    }
+// class ViewTodosUser extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.sortByABC = this.sortByABC.bind(this);  
+//       this.changeFunc = this.changeFunc.bind(this);  
+//       this.data={
+//         listTitles:[]
+//       };
+//     }
 
     
-   changeFunc() {
-    var selectBox = document.getElementById("selectBox");
-    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
-    if(selectedValue=="menu_order"){
-      return;
-    } else if(selectedValue=="alphabetical"){
-      console.log(this.data.listTitles);
+//    changeFunc() {
+//     var selectBox = document.getElementById("selectBox");
+//     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+//     if(selectedValue=="menu_order"){
+//       return;
+//     } else if(selectedValue=="alphabetical"){
+//       console.log(this.data.listTitles);
 
-      this.data.listTitles.sort();
-      console.log(this.data.listTitles);
-      this.render();
-    }
+//       this.data.listTitles.sort();
+//       console.log(this.data.listTitles);
+//       this.render();
+//     }
       
-    alert(selectedValue);
-   }
-    sortByABC(){
-      console.log(this.props.listTodos);
-      var arrayTitles=[];
-      for (let index = 0; index < this.props.listTodos.length; index++) {
-        arrayTitles.push(this.props.listTodos[index].title);
-       } 
-      arrayTitles.sort();
-      console.log(arrayTitles);
+//     alert(selectedValue);
+//    }
+//     sortByABC(){
+//       console.log(this.props.listTodos);
+//       var arrayTitles=[];
+//       for (let index = 0; index < this.props.listTodos.length; index++) {
+//         arrayTitles.push(this.props.listTodos[index].title);
+//        } 
+//       arrayTitles.sort();
+//       console.log(arrayTitles);
 
-    }
+//     }
 
-    render() {
-      return (  
-        <div>
-              <select id="selectBox" name="orderby" class="orderby" onChange={this.changeFunc} autofocus>
-                <option value="menu_order"  selected='selected' >Sorting by default</option>
-                <option value="alphabetical" >Alphabetical order</option>
-                <option value="done" >Todos Done</option>
-              </select>
-              <div>
-             </div>
-            {this.props.listTodos.map((todo)=>(
-              this.data.listTitles.push(todo.title),
-              <div>
-                <p><input id="checkBox" type="checkbox" defaultChecked={todo.completed}/>{todo.title}</p>             
-              </div> 
-          ))}
-        </div>
-      );
-    }
-  }
+//     render() {
+//       return (  
+//         <div>
+//               <select id="selectBox" name="orderby" class="orderby" onChange={this.changeFunc} autofocus>
+//                 <option value="menu_order"  selected='selected' >Sorting by default</option>
+//                 <option value="alphabetical" >Alphabetical order</option>
+//                 <option value="done" >Todos Done</option>
+//               </select>
+//               <div>
+//              </div>
+//             {this.props.listTodos.map((todo)=>(
+//               this.data.listTitles.push(todo.title),
+//               <div>
+//                 <p><input id="checkBox" type="checkbox" defaultChecked={todo.completed}/>{todo.title}</p>             
+//               </div> 
+//           ))}
+//         </div>
+//       );
+//     }
+//   }
    
-  export default ViewTodosUser;
+//   export default ViewTodosUser;
+
+// import React, { useState } from "react";
+
+// export default function ViewTodosUser({ listTodos }) {
+//   const [listTitles, setListTitles] = useState([]);
+
+//   const changeFunc = () => {
+//     const selectBox = document.getElementById("selectBox");
+//     const selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    
+//     if (selectedValue === "menu_order") {
+//       return;
+//     } else if (selectedValue === "alphabetical") {
+//       const sortedTitles = [...listTitles].sort();
+//       setListTitles(sortedTitles);
+//     }
+    
+//     alert(selectedValue);
+//   };
+
+//   const sortByABC = () => {
+//     const arrayTitles = listTodos.map(todo => todo.title);
+//     const sortedTitles = [...arrayTitles].sort();
+//     setListTitles(sortedTitles);
+//   };
+
+//   return (
+//     <div>
+//       <select id="selectBox" name="orderby" className="orderby" onChange={changeFunc} autoFocus>
+//         <option value="menu_order" selected="selected">Sorting by default</option>
+//         <option value="alphabetical">Alphabetical order</option>
+//         <option value="done">Todos Done</option>
+//       </select>
+//       <div>
+//         {listTodos.map((todo) => (
+//           <div key={todo.id}>
+//             <p>
+//               <input id="checkBox" type="checkbox" defaultChecked={todo.completed} />
+//               {todo.title}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+
+import React, { useState } from "react";
+
+const ViewTodosUser = ({ listTodos }) => {
+  const [sortedTodos, setSortedTodos] = useState([...listTodos]);
+
+  const changeFunc = () => {
+    const selectBox = document.getElementById("selectBox");
+    const selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    
+    let sortedList = [...listTodos];
+
+    if (selectedValue === "serial") {
+      // Sort by serial
+      sortedList.sort((a, b) => a.id - b.id);
+    } else if (selectedValue === "performance") {
+      // Sort by performance (completed tasks first)
+      sortedList.sort((a, b) => {
+        if (a.completed && !b.completed) {
+          return -1;
+        } else if (!a.completed && b.completed) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+    } else if (selectedValue === "alphabetical") {
+      // Sort alphabetically by title
+      sortedList.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (selectedValue === "random") {
+      // Sort randomly
+      sortedList.sort(() => Math.random() - 0.5);
+    }
+
+    setSortedTodos(sortedList);
+  };
+
+  return (
+    <div>
+      <select id="selectBox" name="orderby" className="orderby" onChange={changeFunc} autoFocus>
+        <option value="serial">Sort by Serial</option>
+        <option value="performance">Sort by Performance</option>
+        <option value="alphabetical">Sort Alphabetically</option>
+        <option value="random">Sort Randomly</option>
+      </select>
+      <div>
+        {sortedTodos.map((todo) => (
+          <div key={todo.id}>
+            <p>
+              <input id="checkBox" type="checkbox" defaultChecked={todo.completed} />
+              {todo.title}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ViewTodosUser;
